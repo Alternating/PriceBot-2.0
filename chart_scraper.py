e5bc71cb934935b57f5f81bc1079681b1edc1617
Alternating <tneelis@gmail.com>
2024-12-18 00:16:02 -0600

    moved cookies to the settings file for ease of use

--- chart_scraper.py
+++ chart_scraper.py
@@ -1,25 +1,13 @@
 from playwright.async_api import async_playwright
 import os
 import time
+import settings
 
 class CloudflareSession:
     def __init__(self):
-        self.cf_cookies = [
-            {
-                "name": "cf_clearance",
-                "value": "JbNKrIkR5zRQGreyJVfPu9QvM3bculv4hhCHc70mC8w-1734501689-1.2.1.1-5.6JmgP5bgco8WUBXDH9NVtc3ZqY62uWw_E.Mu26iIykWpBn1szBBsw6OzA82.zwii52yvmelL4cyd_sFJNMYGFN9onTGcwhPt39ngFdlCzV5jjFDYCLEb0SYd5Lgb8QEOg0Cp9f01Wl_jqPwowYrTsI6UGes.blI2MWV5_jV9iD02lKElI6HniTd9u3tJRf3ntDS.lG5LN.hiA0wR7lKrrKnUd9.LUlwKrl4Bt6vzqTQecoCpnQvJgrSVmUUnI.1xPnsNNwma6yqK3HJ5rYTJ0dpkp3BZOwADUGLNllr.3xeSh.Wo3p6Y9AGk_aRPUfrlaGdB8eye1aSN3r6dJnHQh2HJclPDLSNx018qBQvFbeRiDdRFalKRxv_iQcPkc155bZTcOdgErZFSOj6d5BWj2Xi7UHX3fravfAQvzFsu.1vOeT7p9Z7JuTh_Mq07ea",
-                "domain": ".dexscreener.com",
-                "path": "/"
-            }
-        ]
+        self.cf_cookies = settings.CF_COOKIES
+        self.headers = settings.CF_HEADERS
         
-        self.headers = {
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
-            'accept-language': 'en-US,en;q=0.9',
-            'upgrade-insecure-requests': '1'
-        }
-
 async def capture_chart_async(token_type: str = 'tetsuo'):
     """
     Capture chart for specified token using async Playwright
